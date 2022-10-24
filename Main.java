c693d9fd71038fb79a32959ba7e723b18ee8da7f
Yummikko <ola.chyla0@gmail.com>
2022-10-24 19:55:43 +0200

    created Battleship and main

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import controller.Battleship;
+import util.Input;
+import view.Display;
+
 public class Main {
     public static void main(String[] args) {
-        Controller.playGame();
+        Display display = new Display();
+        Input input = new Input();
+        Battleship battleship = new Battleship(display, input);
+
+        battleship.playGame();
     }
 }
