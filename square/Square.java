923fd85277e557bdea3beb6e5ca8048743cafc88
Pastor28 <dgorz@interia.com>
2022-10-24 14:15:24 +0200

    Added Square.java with constructor, getters

--- square/Square.java
+++ square/Square.java
@@ -1,4 +1,29 @@
 package square;
 
 public class Square {
+    private final int X;
+    private final int Y;
+    private SquareStatus squareStatus;
+
+    public Square(int x, int y, SquareStatus squareStatus) {
+        X = x;
+        Y = y;
+        this.squareStatus = squareStatus;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public SquareStatus getSquareStatus() {
+        return squareStatus;
+    }
+
+
 }
+
+
