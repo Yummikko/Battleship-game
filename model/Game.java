60f429f6e67d24741562107251aef117c52bb932
Yummikko <ola.chyla0@gmail.com>
2022-10-26 23:15:14 +0200

    small visual changes

--- model/Game.java
+++ model/Game.java
@@ -15,16 +15,32 @@ public class Game {
         this.INPUT = input;
     }
 
+    public void endTurn(){
+        DISPLAY.printBlueMessages("\nClick enter to end Your turn.");
+        INPUT.clickToContinue();
+    }
+
     public void newGame(){
         Integer oceanSize = INPUT.getOceanSize();
-        player1Board = new Board(oceanSize);
-        player2Board = new Board(oceanSize);
-        player1Board.initOcean();
-        player2Board.initOcean();
 
+        player1Board = new Board(oceanSize);
+        player1Board.initOcean();
         DISPLAY.showBoard(player1Board.getOcean());
+        DISPLAY.showShips();
+        DISPLAY.shipSizeVisualisation();
+        //place ships function
+        endTurn();
+        DISPLAY.waitingScreen();
+        INPUT.clickToContinue();
+
+        player2Board = new Board(oceanSize);
+        player2Board.initOcean();
         DISPLAY.showBoard(player2Board.getOcean());
         DISPLAY.showShips();
         DISPLAY.shipSizeVisualisation();
+        endTurn();
+        DISPLAY.shootingPhase();
+        //place ship function
+
     }
 }
